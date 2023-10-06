795329e7b28b078e2fe5e54c6698942e6c10c797
firomsa <assefafiromsa@gmail.com>
2023-10-07 01:26:18 +0300

    restructuring

--- insert-for-DEMO.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO User (FirstName, LastName, Email, Password, RegistrationDate, BankName, AccountHolderName, AccountNumber)
-VALUES
-    ('John', 'Doe', 'johndoe@example.com', 'password123', '2023-01-01', 'Bank A', 'John Doe', '123456'),
-    ('Jane', 'Smith', 'janesmith@example.com', 'password456', '2023-01-02', 'Bank B', 'Jane Smith', '09876'),
-    ('Mike', 'Johnson', 'mikejohnson@example.com', 'password789', '2023-01-03', 'Bank C', 'Mike Johnson', '2468'),
-    ('Emily', 'Davis', 'emilydavis@example.com', 'passwordabc', '2023-01-04', 'Bank A', 'Emily Davis', '13579'),
-    ('David', 'Wilson', 'davidwilson@example.com', 'passworddef', '2023-01-05', 'Bank B', 'David Wilson', '9876'),
-    ('Sarah', 'Brown', 'sarahbrown@example.com', 'passwordghi', '2023-01-06', 'Bank C', 'Sarah Brown', '56789'),
-    ('Michael', 'Taylor', 'michaeltaylor@example.com', 'passwordjkl', '2023-01-07', 'Bank A', 'Michael Taylor', '0123');
