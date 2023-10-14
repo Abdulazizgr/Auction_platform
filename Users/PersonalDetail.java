c1d8bf2aa0b47dcbeff55711db8dc86a971bb003
Samuel Keno <sgere195@gmail.com>
2023-10-14 23:59:02 +0300

    personal detail page handeled

--- Users/PersonalDetail.java
+++ Users/PersonalDetail.java
@@ -15,8 +15,6 @@ import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
 import DAO.*;
-import DAO.User;
-import DAO.UserDAO;
 
 public class PersonalDetail extends JPanel {
     public JLabel Intro, fname, lname, e_mail, reg_date, fanswer, lanswer, eanswer, ranswer;
@@ -25,12 +23,9 @@ public class PersonalDetail extends JPanel {
     public UserDAO udao;
     public User u;
 
-    PersonalDetail(int ID) throws Exception {
-
-        home_personal_content = new JPanel();
-        home_personal_content.setLayout(null);
-        home_personal_content.setBackground(new Color(167, 192, 232));
-        home_personal_content.setPreferredSize(new Dimension(1060, 500));
+    PersonalDetail(int ID) throws SQLException {
+        Border glassyBorder = new BorderUIResource(
+                BorderFactory.createEtchedBorder(EtchedBorder.RAISED, Color.WHITE, new Color(180, 180, 180)));
 
         u = null;
         udao = new UserDAOImplementation();
@@ -41,46 +36,55 @@ public class PersonalDetail extends JPanel {
         rreg_date = u.getRegistrationDate();
 
         Intro = new JLabel("User Details");
+        Intro.setForeground(new Color(0, 0, 255));
+        Intro.setFont(new Font("Arial", Font.PLAIN, 25));
+        Intro.setBackground(new Color(35, 59, 97));
+        Intro.setOpaque(true);
+        Intro.setForeground(Color.white);
+        Intro.setHorizontalAlignment(JLabel.CENTER);
+        Intro.setBorder(glassyBorder);
         Intro.setBounds(100, 50, 200, 50);
-        home_personal_content.add(Intro);
+        add(Intro);
+        setLayout(null);
+        setBounds(0, 0, 1060, 564);
 
         fname = new JLabel("First Name");
         fname.setBounds(50, 100, 100, 50);
-        home_personal_content.add(fname);
+        add(fname);
 
         fanswer = new JLabel();
         fanswer.setText(ffname);
         fanswer.setBounds(300, 100, 100, 50);
-        home_personal_content.add(fanswer);
+        add(fanswer);
 
         lname = new JLabel("Last Name");
         lname.setBounds(50, 150, 100, 50);
-        home_personal_content.add(lname);
+        add(lname);
 
         lanswer = new JLabel();
         lanswer.setText(llname);
         lanswer.setBounds(300, 150, 100, 50);
-        home_personal_content.add(lanswer);
+        add(lanswer);
 
         e_mail = new JLabel("Email");
         e_mail.setBounds(50, 200, 100, 50);
-        home_personal_content.add(e_mail);
+        add(e_mail);
 
         eanswer = new JLabel(ee_mail);
         eanswer.setText(ee_mail);
         eanswer.setBounds(300, 200, 100, 50);
-        home_personal_content.add(eanswer);
+        add(eanswer);
 
         reg_date = new JLabel("Registration Date");
         reg_date.setBounds(50, 250, 100, 50);
-        home_personal_content.add(reg_date);
+        add(reg_date);
 
         ranswer = new JLabel();
         ranswer.setText(rreg_date);
         ranswer.setBounds(300, 250, 100, 50);
-        home_personal_content.add(ranswer);
+        add(ranswer);
 
-        home_personal_content.setVisible(true);
+        setVisible(true);
     }
 
     // public static void main(String args[]) throws Exception {
